0f627a5278e00ed9630dfda6e92e5833dddf3423
makhles <mr.lange85@gmail.com>
2015-06-17 17:03:33 -0300

    Added .gitignore file and renamed main.cpp to test.cpp.

---
+++ test.cpp
@@ -0,0 +1,31 @@
+// This file was created just for testing and debugging.
+
+#include <iostream>
+#include <assert.h>
+#include "NoRB.hpp"
+
+#define NELEM 10
+
+int main(int argc, char* argv[]) {
+
+    NoRB<int>* arv = new NoRB<int>(5);
+
+    std::cout << "Starting..." << std::endl;
+
+    int i;
+    for (i = 0; i < NELEM; i++) {
+        arv = arv->inserir(i, arv);
+    }
+
+    arv->emOrdem(arv);
+    std::vector<NoRB<int>* > elementos = arv->getElementos();
+    
+    std::cout << "Elementos:" << std::endl;
+    for (i = 0; i < NELEM+1; i++) {
+        std::cout << *(elementos[i]->getDado()) << " ";
+    }
+    std::cout << std::endl;
+
+    delete arv;
+    return 0;
+}
